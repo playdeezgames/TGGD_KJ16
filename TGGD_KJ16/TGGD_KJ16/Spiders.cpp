c17ec50958242b40f1ae47c528981b8f57304a2b
ernestpazera <ernestpazera@gmail.com>
2022-01-30 04:49:24 -0600

    spider stats are in

--- TGGD_KJ16/TGGD_KJ16/Spiders.cpp
+++ TGGD_KJ16/TGGD_KJ16/Spiders.cpp
@@ -3,6 +3,7 @@
 #include <ranges>
 #include <map>
 #include <tuple>
+#include <algorithm>
 struct Spider
 {
 	int hp;
@@ -18,5 +19,36 @@ void Spiders::Start()
 			Spider spider = { 1 };
 			spiders.emplace(std::make_tuple(x,y), spider);
 		}
+	}
+}
+bool Spiders::Exists(int x, int y)
+{
+	auto iter = 
+		std::find_if(
+			spiders.begin(), 
+			spiders.end(), 
+			[x,y](const auto& entry) 
+			{
+				return x == std::get<0>(entry.first) && y == std::get<1>(entry.first);
+			});
+	return iter != spiders.end();
+}
+
+bool Spiders::IsAlive(int x, int y)
+{
+	auto iter = spiders.find(std::make_tuple(x, y));
+	if (iter != spiders.end())
+	{
+		return iter->second.hp > 0;
+	}
+	return false;
+}
+
+void Spiders::Kill(int x, int y)
+{
+	auto iter = spiders.find(std::make_tuple(x, y));
+	if (iter != spiders.end())
+	{
+		iter->second.hp = 0;
 	}
 }
--- TGGD_KJ16/TGGD_KJ16/Spiders.h
+++ TGGD_KJ16/TGGD_KJ16/Spiders.h
@@ -3,4 +3,7 @@ struct Spiders
 {
 	Spiders() = delete;
 	static void Start();
+	static bool Exists(int x, int y);
+	static bool IsAlive(int x, int y);
+	static void Kill(int x, int y);
 };
